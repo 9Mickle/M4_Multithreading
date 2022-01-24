9ac759ca04e4d1c880d9776cb71096b42710d710
Mikhail Zaks <mikhail_zaks@epam.com>
2022-01-24 20:57:18 +0300

    update tests

--- src/test/java/com/epam/task/TestApp.java
+++ src/test/java/com/epam/task/TestApp.java
@@ -1,33 +1,31 @@
 package com.epam.task;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.*;
 
 public class TestApp {
 
     private static BlockingQueue<String> queue;
     private static ConcurrentHashMap<String, List<String>> map;
-    private static boolean flag;
+    private static ExecutorService executorService;
+    private static List<Future<ConcurrentHashMap<String, List<String>>>> futures;
 
     @BeforeAll
     public static void setUp() throws InterruptedException {
         queue = new LinkedBlockingQueue<>(5);
         map = new ConcurrentHashMap<>();
 
-        ExecutorService executorService = Executors.newCachedThreadPool();
+        List<Callable<ConcurrentHashMap<String, List<String>>>> taskList =
+                List.of(new Producer(queue), new Consumer(queue, map), new Consumer2(queue, map));
 
-        executorService.execute(new Producer(queue));
-        executorService.execute(new Consumer(queue, map));
-        executorService.execute(new Consumer2(queue, map));
-
+        executorService = Executors.newCachedThreadPool();
+        futures = executorService.invokeAll(taskList);
         executorService.shutdown();
-        flag = executorService.awaitTermination(5, TimeUnit.SECONDS);
-        queue.clear();
     }
 
     /**
@@ -45,11 +43,10 @@ public class TestApp {
      */
     @Test
     public void checkCountElemInMap() {
-        if (flag) {
-            int expected = 2;
-            int actual = map.size();
-            Assertions.assertEquals(expected, actual);
-        }
+        int expected = 2;
+        int actual = map.size();
+        Assertions.assertFalse(map.isEmpty());
+        Assertions.assertEquals(expected, actual);
     }
 
     /**
@@ -57,21 +54,36 @@ public class TestApp {
      */
     @Test
     public void checkCountWords() {
-        if (flag) {
-            int expected = 14384;
+        int expected = 14384;
+        int countWords1 = Integer.parseInt(map.get("cons1").get(0));
+        int countWords2 = Integer.parseInt(map.get("cons2").get(0));
+        int actual = countWords1 + countWords2;
+        Assertions.assertEquals(expected, actual);
+    }
 
-            int countWords1 = map.get("cons1")
-                    .stream()
-                    .map(s -> s.split(" ").length)
-                    .reduce(0, Integer::sum);
+    @Test
+    public void checkQueue() {
+        Assertions.assertTrue(queue.isEmpty());
+        Assertions.assertThrows(NoSuchElementException.class, () -> queue.element());
+        Assertions.assertNull(queue.peek());
 
-            int countWords2 = map.get("cons2")
-                    .stream()
-                    .map(s -> s.split(" ").length)
-                    .reduce(0, Integer::sum);
+        // Переполнение очереди.
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            for (int i = 0; i < 6; i++) {
+                queue.add(String.valueOf(i));
+            }
+        });
+    }
 
-            int actual = countWords1 + countWords2;
-            Assertions.assertEquals(expected, actual);
-        }
+    @Test
+    public void checkExecutorService() {
+        Assertions.assertTrue(executorService.isShutdown());
+        Assertions.assertTrue(executorService.isTerminated());
+    }
+
+    @Test
+    public void checkFuturesList() {
+        Assertions.assertFalse(futures.isEmpty());
+        Assertions.assertEquals(3, futures.size());
     }
 }
